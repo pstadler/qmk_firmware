0d1e19f56ec8dc22367a1ae3003697e1ff0f5768
Patrick Stadler <patrick.stadler@gmail.com>
2020-06-30 16:20:13 +0200

    change rhug-iso-63

--- keyboards/dz60/keymaps/rhug-iso-63/keymap.c
+++ keyboards/dz60/keymaps/rhug-iso-63/keymap.c
@@ -17,9 +17,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,
     M_FN,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,
     KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP,   M_FN,
-    KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
+    KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
   ),
-
+  // FN
   [_FL] = LAYOUT_60_iso_split_rshift(
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______,                            _______,                            _______, _______, _______, _______
   ),
-
+  // FN+Shift
   [_ML] = LAYOUT_60_iso_split_rshift(
     _______, KC_BRMD, KC_BRMU, _______, _______, _______, _______, KC_MRWD, KC_MPLY, KC_MFFD, KC_MUTE, KC_VOLD, KC_VOLU, RESET,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
